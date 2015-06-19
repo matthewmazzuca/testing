4b2de2597e5b6173dd8710e4637fbf86aab760ae
@anulman <anulman@gmail.com>
2015-06-19 10:46:30 -0400

    updated db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -28,54 +28,6 @@ ActiveRecord::Schema.define(version: 20150612025412) do
 
   add_index "beacons", ["location_id"], name: "index_beacons_on_location_id", using: :btree
 
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
-
-  create_table "orders", force: :cascade do |t|
-    t.integer  "user_id"
-    t.decimal  "total"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
-
-  create_table "placements", force: :cascade do |t|
-    t.integer  "order_id"
-    t.integer  "product_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "quantity",   default: 0
-  end
-
-  add_index "placements", ["order_id"], name: "index_placements_on_order_id", using: :btree
-  add_index "placements", ["product_id"], name: "index_placements_on_product_id", using: :btree
-
-  create_table "products", force: :cascade do |t|
-    t.string   "title",      default: ""
-    t.decimal  "price",      default: 0.0
-    t.boolean  "published",  default: false
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "quantity",   default: 0
-  end
-
-  add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
-
   create_table "properties", force: :cascade do |t|
     t.string   "name"
     t.string   "address"

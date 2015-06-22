2e4e4c9be18ef33d450dd70868ac5f310c8edf48
Matthew Mazzuca <matthew.mazzuca@mail.utoronto.ca>
2015-06-22 14:22:04 -0400

    cleanup

--- app/mailers/order_mailer.rb
+++
@@ -1,9 +0,0 @@
-class OrderMailer < ActionMailer::Base
-  default from: "no-reply@openhouse.com"
-
-  def send_confirmation(order)
-    @order = order
-    @user = @order.user
-    mail to: @user.email, subject: "Order Confirmation"
-  end
-end

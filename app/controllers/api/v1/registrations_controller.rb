565a46a93e10ffee0939f1e9164e8d15683b8a58
Matthew Mazzuca <matthew.mazzuca@mail.utoronto.ca>
2015-06-24 14:40:35 -0400

    fixed error with registrations controller

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,6 +1,28 @@
-class User
+class  User
 	class Api::V1::RegistrationsController < Devise::RegistrationsController
 
-		
+
+		skip_before_filter :verify_authenticity_token
+			respond_to :json
+
+			def register
+		    build_resource(sign_up_params)
+		    if resource.save
+		      render :json => {:result => 'OK'}
+		    else
+		      render :json => {:result => 'ERROR'}
+		  end
+		end
+			
+		private
+
+		def sign_up_params
+			params.require(:user).permit(:email, :password, :password_confirmation)
+		end
+
+		def account_update_params
+			params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+		end
+
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ require 'api_constraints'
 
 OpenhouseApi::Application.routes.draw do
   # nobody puts baby in a namespace
+  devise_scope :user do
+    post "api/v1/register" => "api/v1/registration#register"
+  end
+
   devise_for :users, 
     path: '/api/v1/users',
-    controllers: { sessions: 'api/v1/sessions', :registrations => "users/registrations" } do
+    controllers: { sessions: 'api/v1/sessions', :registration => "registrations" } do
       get "api/v1/users/sign_up" => "users/registrations#new", :as => :user_signup
       get "api/v1/users/sign_in" => "api/v1/sessions", :as => :user_signin
   end
